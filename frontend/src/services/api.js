c82eca2ac62944eff877d2a1474acb41c957dfc0
sumitrock <sumitrock2552@gmail.com>
2026-03-26 13:47:06 +0530

    Fix baseURL add /api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://threed-viewer-backend-wqxw.onrender.com/'
+  baseURL: 'https://threed-viewer-backend-wqxw.onrender.com/api'
 })
 
 API.interceptors.request.use((req) => {
